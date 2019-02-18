e72cdb5a8db43476f5475095f32c06700c85fb97
Artyom Sadyrin <asadyryn@icloud.com>
2019-02-18 11:57:54 +0300

    Created project, Added textField, button and table view, embedded in Stack View

---
+++ Search List/Search List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Search List
+//
+//  Created by Artyom Sadyrin on 2/18/19.
+//  Copyright © 2019 Artyom Sadyrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
