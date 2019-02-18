8bfe521328d8348f91517c1b786f25264c795e2b
Artyom Sadyrin <asadyryn@icloud.com>
2019-02-18 12:54:04 +0300

    Fixed some outlets, added temp data for tableView, added border color to button

--- Search List/Search List/ViewController.swift
+++ Search List/Search List/ViewController.swift
@@ -8,21 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchResultTableView.dataSource = self
+        searchResultTableView.delegate = self
+        
     }
 
-    @IBAction func inputForSearchTextField(_ sender: UITextField) {
-    }
+    // MARK: Properties
     
-    @IBAction func googleSearchButton(_ sender: UIButton) {
-    }
+    @IBOutlet weak var inputForSearchTextField: UITextField!
+    
+    @IBOutlet weak var googleSearchButtonOutlet: UIButton!
     
     @IBOutlet weak var searchResultTableView: UITableView!
     
+    private var searchResults = ["link 1 from search result", "link 2 from search result", "link 3 from search result"]
+    
+    // MARK: Table View Data Source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? SearchResultTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of SearchResultTableViewCell.")
+        }
+        
+        let link = searchResults[indexPath.row]
+        
+        cell.linkLabel.text = link
+        
+        return cell
+    }
+    
+    // MARK: Search Actions
+    
+    @IBAction func googleSearchButton(_ sender: UIButton) {
+        
+        
+        
+    }
     
 }
+
+extension UIButton
+{
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
 
